326ff873db8274b3d78721ba5e24a964dc584315
Flavio Cardoso <flavioc401@gmail.com>
2022-10-13 01:05:39 -0300

    Add files via upload

---
+++ teste/Teste.java
@@ -0,0 +1,11 @@
+package br.com.teste;
+
+import br.com.flaviocardoso.escola.modelo.*;
+
+public class Teste {
+	public static void main(String[] args) {
+		Aluno a = new Aluno("Flavio");
+		
+		System.out.println(a.getNomeAluno());
+	}
+}
