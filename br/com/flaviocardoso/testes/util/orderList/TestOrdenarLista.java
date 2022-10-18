126e401a060afbfe6b3f3f923c45a367a5d12192
Flavio Cardoso <flavioc401@gmail.com>
2022-10-18 01:53:54 -0300

    Atualizar order lista

--- br/com/flaviocardoso/testes/util/orderList/TestOrdenarLista.java
+++ br/com/flaviocardoso/testes/util/orderList/TestOrdenarLista.java
@@ -1,11 +1,10 @@
 package br.com.flaviocardoso.testes.util.orderList;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import br.com.flaviocardoso.banco.Cliente;
 import br.com.flaviocardoso.banco.Conta;
 import br.com.flaviocardoso.banco.ContaCorrente;
 import br.com.flaviocardoso.banco.ContaPoupanca;
@@ -16,15 +15,27 @@ public class TestOrdenarLista {
 		
 		try {
 			Conta cc1 = new ContaCorrente(13, 66);
+			Cliente cl1 = new Cliente();
+			cl1.setNome("Flavio");
+			cc1.setTitular(cl1);
 			cc1.depositar(343.0);
 			
 			Conta cc2 = new ContaPoupanca(14, 28);
+			Cliente cl2 = new Cliente();
+			cl2.setNome("Cardoso");
+			cc2.setTitular(cl2);
 			cc2.depositar(545.0);
-			
+//			
 			Conta cc3 = new ContaCorrente(34, 65);
+			Cliente cl3 = new Cliente();
+			cl3.setNome("Tiago");
+			cc3.setTitular(cl3);
 			cc3.depositar(453.0);
 			
 			Conta cc4 = new ContaPoupanca(36, 93);
+			Cliente cl4 = new Cliente();
+			cl4.setNome("Wagner");
+			cc4.setTitular(cl4);
 			cc4.depositar(564.0);
 			
 			List<Conta> lista = new ArrayList<Conta>();
@@ -39,59 +50,47 @@ public class TestOrdenarLista {
 			
 			lista.sort(comparadorNumero);
 			
-			loop(lista, "numero");
+			for (Conta c : lista) {
+				System.out.println(c.getNumero());
+			}
 			
 			System.out.println("----Organiza Conta por agencia----");
 			
 			AgenciaContaCompardor comparadorAgencia = new AgenciaContaCompardor();
 
 			lista.sort(comparadorAgencia);
-			loop(lista, "agencia");
+			
+			for (Conta c : lista) {
+				System.out.println(c.getAgencia());
+			}
 			
 			System.out.println("----Organiza Conta por saldo----");
 			
 			SaldoContaCompardor comparadorSaldo = new SaldoContaCompardor();
 
 			lista.sort(comparadorSaldo);
-			loop(lista, "saldo");
+			
+			for (Conta c: lista) {
+				System.out.println(c.getSaldo());
+			}
+			
+			System.out.println("---Organiza Conta por titular nome----");
+			
+			TitularContaComparator comparadorTitularNome = new TitularContaComparator();
+			
+			lista.sort(comparadorTitularNome);
+			
+			for (Conta c: lista) {
+				System.out.println(c.getTitular().getNome());
+			}
 			
 			
 		} catch (NaoPodeComValorNegativoException e) {
 			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 	
-	public static void loop(List<Conta> lista, String attribute) 
-			throws 
-				NoSuchMethodException, 
-				SecurityException, 
-				IllegalAccessException, 
-				IllegalArgumentException, 
-				InvocationTargetException {
-		String valor = "get" + attribute.substring(0,1).toUpperCase() + attribute.substring(1);
-		
-		for(Conta c : lista) {
-			Class<? extends Conta> pegaClasse = c.getClass();
-			Method method = pegaClasse.getMethod("get" + valor.substring(0,1).toUpperCase() + valor.substring(1), null);
-			System.out.println(method.invoke(c, new Object[0]));
-//			System.out.println(valor.substring(0,1).toUpperCase() + valor.substring(1));
-		}
-	}
+	
 	
 }
 
@@ -118,6 +117,18 @@ class SaldoContaCompardor implements Comparator<Conta> {
 	@Override
 	public int compare(Conta c1, Conta c2) {
 		return Integer.valueOf((int) (c1.getSaldo() - c2.getSaldo())) ;
+	}
+	
+}
+
+class TitularContaComparator implements Comparator<Conta> {
+
+	@Override
+	public int compare(Conta c1, Conta c2) {
+		String nomeC1 = c1.getTitular().getNome();
+		String nomeC2 = c2.getTitular().getNome();
+		
+		return nomeC1.compareTo(nomeC2);
 	}
 	
 }
